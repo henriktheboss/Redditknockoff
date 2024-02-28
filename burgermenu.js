b5c3759f658a66d5e10c74922fc9cf6086d55a55
henriktheboss <122083502+henriktheboss@users.noreply.github.com>
2024-02-28 11:12:24 +0100

    lagde profil sidepanel (kan ikke lukkes enda)

--- burgermenu.js
+++ burgermenu.js
@@ -2,11 +2,26 @@ document.getElementById('hamburgerIcon').addEventListener('click', function() {
     toggleSidePanel();
 });
 
+document.getElementById('profil').addEventListener('click', function() {
+    toggleProfilSidepannel();
+});
+
 function toggleSidePanel() {
     var sidepanel = document.getElementById('sidepanel');
     if (sidepanel.style.width === '250px') {
         sidepanel.style.width = '0';
     } else {
         sidepanel.style.width = '250px';
+    }
+}
+
+
+function toggleProfilSidepannel() {
+    var sidepanel = document.getElementById('profil-sidepannel');
+    if (sidepanel.style.width === '250px') {
+        //sidepanel.style.width = '0';
+    } else {
+        sidepanel.style.transition = 'all 0.5s ease-in-out'
+        sidepanel.style.transform = "translateX(0px)"
     }
 }
